896b21bb30a28b481da1917f0d58b17d9edf5fce
Scott Molloy <molloys@oregonstate.edu>
2016-03-31 02:11:54 -0700

    Adding initial content and style for assignment 2

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 var express = require('express');
+var path = require('path');
 var app = express();
 app.set('port', (process.env.PORT || 3000));
 
 app.get('/', function(req, res) {
-  res.send('Hello World!');
+  res.sendFile(path.join(__dirname, 'content.html'));
+});
+
+app.get('/style.css', function(req, res) {
+  res.sendFile(path.join(__dirname, 'style.css'));
 });
 
 var server = app.listen(app.get('port'), function() {
