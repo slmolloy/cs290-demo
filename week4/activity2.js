72029c4ed3462a92a5f8327a3a4529e95884bb14
Scott Molloy <molloys@oregonstate.edu>
2016-04-20 20:44:35 -0700

    Second pass at activity2
    Use bind method on function to bind this context as well as item string
    and item value at given index.

--- week4/activity2.js
+++ week4/activity2.js
@@ -1,15 +1,12 @@
-function displayList() {
-  console.log(this.item + ' ' + this.index);
+function displayList(item, index) {
+  console.log(item + ' ' + index);
 }
 
 function buildList(list) {
   var result = [];
   for (var i = 0; i < list.length; i++) {
     var item = 'item' + list[i];
-    result.push({
-      item: item,
-      index: list[i],
-      func: displayList});
+    result.push(displayList.bind(this, item, list[i]));
   }
   return result;
 }
@@ -17,7 +14,7 @@ function buildList(list) {
 function testList() {
   fnlist = buildList([1,2,3]);
   for (var j = 0; j < fnlist.length; j++) {
-    fnlist[j].func();
+    fnlist[j]();
   }
 }
 
